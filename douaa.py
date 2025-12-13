2d315c828e753733ec11113a486dfbdbba21d53d
bouchra59 <138511159+bouchra59@users.noreply.github.com>
2025-12-13 13:13:21 +0000

    Correction du script douaa.py et ajout de la sauvegarde CSV

--- douaa.py
+++ douaa.py
@@ -16,25 +16,15 @@ data = {
 }
 df = pd.DataFrame(data)
 
-# Affichage du tableau
-print(df)
-GTACGTAGC",
-        "TTAACCGGAT"
-    ],
-    "Longueur": [12, 12, 12, 10, 11, 10, 10],
-    "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
-}
-
-df = pd.DataFrame(data)
-# Affichage du tableau
-print(df)
-
-    "Longueur": [12, 12, 12, 10, 11, 10, 10],
-    "Pourcentage_GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
-}
-
-df = pd.DataFrame(data)
-
 print("Tableau initial :")
 print(df)
 print("\n-----------------------------\n")
+# Question 2 : Afficher la colonne Longueur
+print(df["Longueur"])
+# Question 3 : Filtrer les séquences de longueur > 10
+seq_longues = df[df["Longueur"] > 10]
+print(seq_longues)
+
+# Sauvegarder le DataFrame dans un fichier CSV
+df.to_csv('sequences.csv', index=False)
+print("\nLes informations ont été sauvegardées dans 'sequences.csv'")
